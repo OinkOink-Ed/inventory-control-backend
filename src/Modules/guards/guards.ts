2ca00d7f8d5d24d7afd8b0b0b053aa6f9437eee3
root <root@DESKTOP-55FODD9.>
2024-11-18 00:48:02 +0300

    stage

--- src/Modules/guards/guards.ts
+++ src/Modules/guards/guards.ts
@@ -2,7 +2,9 @@ import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+
     canActivate(context: ExecutionContext): boolean {
+
         const request = context.switchToHttp().getRequest();
         return request.headers.authorization === 'secret';
     };
