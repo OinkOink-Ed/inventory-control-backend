48ef64570c5ce8aeeda679a990e8b4bf192a8aec
root <root@DESKTOP-55FODD9.>
2025-04-08 23:41:48 +0300

    stage

--- src/common/config/configuration.ts
+++ src/common/config/configuration.ts
@@ -14,16 +14,17 @@ export interface DatabaseConfig {
 export default (): DatabaseConfig => ({
     port: parseInt(process.env.APP_PORT, 10) || 3000,
     database: {
-        host: process.env.DB_HOST,
+        host: process.env.DB_HOST || "localhost",
         port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        name: process.env.DB_DATABASE
+        user: process.env.DB_USER || "root",
+        password: process.env.DB_PASSWORD || "password1",
+        name: process.env.DB_DATABASE || "inventory_control"
     },
 });
 
 export const validationSchema = Joi.object({
-    PORT: Joi.number().port().default(3000),
+    JWT_SECRET: Joi.string(),
+    APP_PORT: Joi.number().port().default(3000),
     DB_USER: Joi.string().default("root"),
     DB_PASSWORD: Joi.string().default("password1"),
     DB_DATABASE: Joi.string().default("inventory_control"),
